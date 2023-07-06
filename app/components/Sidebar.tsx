b452dc61d26aa0507ef7d047f2dd37d3745c4456
crackcodesprogramloads <mikepadial@gmail.com>
2023-07-06 14:13:43 +0700

    Fix sidebar mail link

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -71,13 +71,7 @@ const Sidebar = () => {
           <path d="M15 3h-2a6 6 0 0 0 -6 6h-3v4h3v8h4v-7h4v7h4v-11h-8v-1.033a1.967 1.967 0 0 1 2 -1.967h2v-4z"></path>
         </svg>
       </Link>
-      <Link
-        className="hover:mb-0.5"
-        rel="noopener noreferrer"
-        target="_blank"
-        aria-label="email"
-        href="/contact"
-      >
+      <Link className="hover:mb-0.5" aria-label="email" href="#contact">
         <svg
           stroke="currentColor"
           fill="currentColor"
