8294a8d133f2024f93124e7cbb4e69453b9341ce
crackcodesprogramloads <mikepadial@gmail.com>
2024-03-04 09:43:57 +0700

    Change hero page heading text

--- app/components/sections/hero/HeroSection.tsx
+++ app/components/sections/hero/HeroSection.tsx
@@ -27,12 +27,7 @@ export default function HeroSection() {
       <LazyMotion features={domAnimation}>
         <div className="mt-24 sm:mt-20 md:mt-0 md:relative flex items-center justify-center w-[95%] md:w-3/4 h-full">
           <div className="md:absolute flex flex-col items-center justify-around w-full h-2/3 pt-14 sm:left-24 top-10">
-            <m.div
-              variants={textVariant}
-              initial={"hidden"}
-              animate={"show"}
-              className="flex flex-col gap-4"
-            >
+            <m.div variants={textVariant} initial={"hidden"} animate={"show"} className="flex flex-col gap-4">
               <m.p
                 variants={textVariant}
                 className="text-3xl sm:text-4xl md:text-3xl lg:text-4xl underline underline-offset-2 decoration-1 decoration-yellow-500"
@@ -49,20 +44,14 @@ export default function HeroSection() {
                 variants={textVariant}
                 className="text-3xl sm:text-4xl md:text-3xl lg:text-4xl underline underline-offset-2 decoration-1 decoration-yellow-500"
               >
-                Greater Future
+                Launching Projects
               </m.p>
-              <m.div
-                variants={textVariant}
-                className="mt-8 w-64 h-12 sm:w-64 lg:w-72 rounded-md relative bg-yellow-600 mr-14"
-              >
+              <m.div variants={textVariant} className="mt-8 w-64 h-12 sm:w-64 lg:w-72 rounded-md relative bg-yellow-600 mr-14">
                 <a href="#contact">
                   <button className="w-64 h-12 sm:w-64 lg:w-72 text-xl sm:text-lg lg:text-xl absolute rounded-md bg-sky-950 border border-yellow-600 text-slate-300 hover:text-slate-100 top-[3px] left-[2px] active:top-0 active:left-0">
                     Need code? Let&apos;s Talk!
                     <div className="absolute top-0 right-10">
-                      <LottiePlayer
-                        animationData={GreenMouseClick}
-                        style={ClickStyle}
-                      />
+                      <LottiePlayer animationData={GreenMouseClick} style={ClickStyle} />
                     </div>
                   </button>
                 </a>
